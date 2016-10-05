bba0d80042831104b68ad6ad771ef08d79494938
obliviusm <obliviusm@gmail.com>
2016-10-05 17:48:05 +0300

    add helper for headquarters to get region

---
+++ app/helpers/project-property-headquarters.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+const citiesFromUSA = [
+  'San Francisco',
+  'New York'
+]
+
+export function projectPropertyHeadquarters([city]/*, hash*/) {
+  if ( citiesFromUSA.some( element => city.includes(element) ) ) {
+    return 'USA';
+  }
+  return 'Other World';
+}
+
+export default Ember.Helper.helper(projectPropertyHeadquarters);
