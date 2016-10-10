4c7a00464b944a9bc0549407a8ec0c7c2b2942f1
obliviusm <obliviusm@gmail.com>
2016-10-10 15:23:26 +0300

    add edit page

--- app/routes/projects/edit.js
+++ app/routes/projects/edit.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
+  model(params) {
     return this.store.findRecord('project', params.project_id);
   },
   actions: {
