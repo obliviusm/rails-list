0b9b28c17f17ff1627275d3d799fd287630530c1
obliviusm <obliviusm@gmail.com>
2016-10-14 00:44:01 +0300

    move filter from controller to route

--- app/routes/projects/index.js
+++ app/routes/projects/index.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.get('store').findAll('project');
+  },
+  actions: {
+    filterByCategory(param) {
+      if (param !== '') {
+        return this.get('store').query('project', { filter: { category: param } });
+      } else {
+        return this.get('store').findAll('project');
+      }
+    }
   }
 });
