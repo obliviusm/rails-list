fb88cc780b7f1de6f8fb7e2ab86b3daa3d9d468d
obliviusm <obliviusm@gmail.com>
2016-10-13 16:50:34 +0300

    WIP form validations

--- app/models/project.js
+++ app/models/project.js
@@ -1,6 +1,16 @@
 import DS from 'ember-data';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default DS.Model.extend({
+const Validations = buildValidations({
+  title: {
+    description: 'Title',
+    validators: [
+      validator('presence', true)
+    ]
+  }
+});
+
+export default DS.Model.extend(Validations, {
   title: DS.attr('string'),
   founders: DS.attr('string'),
   headquarters: DS.attr('string'),
