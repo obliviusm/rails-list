b801dfbcfdd7f396ab908d165261aa6960a51ece
obliviusm <obliviusm@gmail.com>
2016-10-16 23:05:30 +0300

    add auth headers to ember date

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,20 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import ENV from '../config/environment';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
+// import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default  DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+export default  DS.JSONAPIAdapter.extend({
   host: ENV.APP.host,
   namespace: 'api',
-  authorizer: 'authorizer:devise'
+  session: Ember.inject.service('session'),
+  headers: Ember.computed('session.authToken', function() {
+    let authProps = this.get('session.data.authenticated');
+    return {
+      "access-token": authProps["accessToken"],
+      client: authProps["client"],
+      expiry: authProps["expiry"],
+      "token-type": authProps["tokenType"],
+      uid: authProps["uid"]
+    };
+  })
 });
