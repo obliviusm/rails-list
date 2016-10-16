9aa61de5667d03a5584844e313ba69729aa81c57
obliviusm <obliviusm@gmail.com>
2016-10-16 20:04:56 +0300

    display protected message from the server

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -5,28 +5,19 @@ import ENV from '../config/environment';
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service('session'),
   model() {
-    // return new RSVP.Promise((resolve, reject) => {
-      let authProps = this.get('session.data.authenticated');
-      let requestOptions = {
-        url:      ENV.APP.host + '/api/demo/members_only',
-        type:     'GET',
-        dataType: 'json',
-        headers: {
-          "access-token": authProps["accessToken"],
-          client: authProps["client"],
-          expiry: authProps["expiry"],
-          "token-type": authProps["tokenType"],
-          uid: authProps["uid"]
-        }
-        // beforeSend(xhr, settings) {
-        //   xhr.setRequestHeader('Accept', settings.accepts.json);
-        //   xhr.setRequestHeader('access-token', settings.accepts.json);
-        //   xhr.setRequestHeader('client', settings.accepts.json);
-        //   xhr.setRequestHeader('expiry', settings.accepts.json);
-        // }
-      };
-      console.log(requestOptions);
-      return jQuery.ajax(requestOptions);
-    // });
+    let authProps = this.get('session.data.authenticated');
+    let requestOptions = {
+      url:      ENV.APP.host + '/api/demo/members_only',
+      type:     'GET',
+      dataType: 'json',
+      headers: {
+        "access-token": authProps["accessToken"],
+        client: authProps["client"],
+        expiry: authProps["expiry"],
+        "token-type": authProps["tokenType"],
+        uid: authProps["uid"]
+      }
+    };
+    return jQuery.ajax(requestOptions);
   }
 });
