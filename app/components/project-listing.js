445116e7ec8aba0a0555faa2706b11b9960aa41a
obliviusm <obliviusm@gmail.com>
2016-10-18 17:18:28 +0300

    show edit link only on user's projects

--- app/components/project-listing.js
+++ app/components/project-listing.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isWide: false,
+  session: Ember.inject.service('session'),
+  currentUserEmail: Ember.computed('session.data.authenticated.uid', function() {
+    return this.get('session.data.authenticated.uid');
+  }),
+  createdByCurrentUser: Ember.computed('currentUserEmail', 'project.userEmail', function() {
+    return this.get('currentUserEmail') == this.get('project.userEmail');
+  }),
   actions: {
     toggleImageSize() {
       this.toggleProperty('isWide');
